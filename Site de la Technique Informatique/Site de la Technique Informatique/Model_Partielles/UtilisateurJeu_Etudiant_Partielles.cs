5e1aaafd63c8341400cee25c4778ee3a16ff16b0
CedricArchambault <cedric.archambault@gmail.com>
2015-02-06 13:35:35 -0500

    inscription

---
+++ Site de la Technique Informatique/Site de la Technique Informatique/Model_Partielles/UtilisateurJeu_Etudiant_Partielles.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Site_de_la_Technique_Informatique.Model_Partielles
+{
+    public class UtilisateurJeu_Etudiant_Partielles
+    {
+    }
+}
---
+++ Site de la Technique Informatique/Site de la Technique Informatique/Model_Partielles/UtilisateurJeu_Partielles.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Site_de_la_Technique_Informatique.Model;
+using System.ComponentModel.DataAnnotations;
+
+namespace Site_de_la_Technique_Informatique.Model
+{
+    public partial class UtilisateurJeu
+    {
+    }
+    //[MetadataType(typeof(UtilisateurJeuValidation))]
+    
+}
