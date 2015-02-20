c7e53f7c74138031d083e4d4c079b7693288936c
xavpoutre1 <poutrexavier@gmail.com>
2015-02-20 13:44:15 -0500

    :O

--- Site de la Technique Informatique/Site de la Technique Informatique/OffreEmploi.aspx.designer.cs
+++ Site de la Technique Informatique/Site de la Technique Informatique/OffreEmploi.aspx.designer.cs
@@ -13,12 +13,120 @@ namespace Site_de_la_Technique_Informatique {
     public partial class OffreEmploi {
         
         /// <summary>
-        /// Contrôle lviewOffreEmploi.
+        /// Contrôle btnRetour.
         /// </summary>
         /// <remarks>
         /// Champ généré automatiquement.
         /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ListView lviewOffreEmploi;
+        protected global::System.Web.UI.WebControls.Button btnRetour;
+        
+        /// <summary>
+        /// Contrôle lblTitreOffre.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblTitreOffre;
+        
+        /// <summary>
+        /// Contrôle lblDescriptionOffre.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblDescriptionOffre;
+        
+        /// <summary>
+        /// Contrôle lblAdresseVille.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblAdresseVille;
+        
+        /// <summary>
+        /// Contrôle lblNbHeureSemaine.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblNbHeureSemaine;
+        
+        /// <summary>
+        /// Contrôle lblDateExpiration.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblDateExpiration;
+        
+        /// <summary>
+        /// Contrôle lblDateDebutOffre.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblDateDebutOffre;
+        
+        /// <summary>
+        /// Contrôle lblSalaire.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblSalaire;
+        
+        /// <summary>
+        /// Contrôle lblNoTelephone.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblNoTelephone;
+        
+        /// <summary>
+        /// Contrôle lblNoTelecopieur.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblNoTelecopieur;
+        
+        /// <summary>
+        /// Contrôle lblCourrielOffre.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblCourrielOffre;
+        
+        /// <summary>
+        /// Contrôle lblPersonneRessource.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblPersonneRessource;
+        
+        /// <summary>
+        /// Contrôle lnkPDF.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton lnkPDF;
     }
 }
