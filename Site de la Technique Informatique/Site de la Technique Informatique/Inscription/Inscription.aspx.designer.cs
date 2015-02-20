914cde6fd43d596f8a2a24241084e3380508420f
CedricArchambault <cedric.archambault@gmail.com>
2015-02-20 08:19:04 -0500

    Incription adapter pour la nouvelle bd

--- Site de la Technique Informatique/Site de la Technique Informatique/Inscription/Inscription.aspx.designer.cs
+++ Site de la Technique Informatique/Site de la Technique Informatique/Inscription/Inscription.aspx.designer.cs
@@ -13,15 +13,6 @@ namespace Site_de_la_Technique_Informatique.Inscription {
     public partial class Inscription {
         
         /// <summary>
-        /// Contrôle ScriptManager1.
-        /// </summary>
-        /// <remarks>
-        /// Champ généré automatiquement.
-        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.ScriptManager ScriptManager1;
-        
-        /// <summary>
         /// Contrôle lviewFormulaireInscription.
         /// </summary>
         /// <remarks>
---
+++ Site de la Technique Informatique/Site de la Technique Informatique/Model_Partielles/Membre_Partielles.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Site_de_la_Technique_Informatique.Model;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace Site_de_la_Technique_Informatique.Model
+{
+    public partial class Membre
+    {
+    }
+    [MetadataType(typeof(MembreValidation))]
+    public partial class Membre : IValidatableObject
+    {
+        public IEnumerable<ValidationResult> Validate(ValidationContext ValidationContext)
+        {
+            LeModelTIContainer leContext = new LeModelTIContainer();
+            var listeRetour = new List<ValidationResult>();
+            return listeRetour;
+        }
+    }
+}
+public partial class MembreValidation
+{
+        [DisplayName("Nom")]
+    [Required(ErrorMessage = "Un nom est requis.")]
+    [StringLength(64, ErrorMessage = "Le nom doit avoir moins de 64 caractères.")]
+    public string nom { get; set; }
+    [DisplayName("Prénom")]
+    [Required(ErrorMessage = "Un prénom est requis.")]
+    [StringLength(64, ErrorMessage = "Le prénom doit avoir moins de 64 caractères.")]
+    public string prenom { get; set; }
+    [DisplayName("Courriel"), Required(ErrorMessage = "Le courriel est requis.")]
+    [RegularExpression("^[-0-9a-zA-Z.+_]+@[-0-9a-zA-Z.+_]+\\.[a-zA-Z]{2,4}", ErrorMessage = "Le courriel doit être valide.")]
+    [StringLength(64, ErrorMessage = "Le courriel doit avoir moins de 64 caractères.")]
+    public string courriel { get; set; }
+    [DisplayName("Mot de passe"),Required(ErrorMessage = "Un mot de passe est requis.")]
+    [StringLengthRange(Minimum = 4, ErrorMessage = "Le mot de passe doit avoir au minimu 4 caractères.")]
+    public string hashMotDepasse { get; set; }
+}
+public class StringLengthRangeAttribute : ValidationAttribute
+{
+    public int Minimum { get; set; }
+    public int Maximum { get; set; }
+
+    public StringLengthRangeAttribute()
+    {
+        this.Minimum = 0;
+        this.Maximum = int.MaxValue;
+    }
+    public override bool IsValid(object value)
+    {
+
+        string strValue = value as string;
+        if (!string.IsNullOrEmpty(strValue))
+        {
+            int len = strValue.Length;
+            return len >= this.Minimum && len <= this.Maximum;
+        }
+        return true;
+    }
+}
