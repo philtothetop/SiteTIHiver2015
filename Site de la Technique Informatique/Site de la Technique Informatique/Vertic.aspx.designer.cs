c8f4bb903a18135a2123f203c9bd32409691c8f7
Jacob Fontaine <jacob.fontaine@gmail.com>
2015-02-27 10:06:01 -0500

    vertic fini

--- Site de la Technique Informatique/Site de la Technique Informatique/Vertic.aspx.designer.cs
+++ Site de la Technique Informatique/Site de la Technique Informatique/Vertic.aspx.designer.cs
@@ -22,33 +22,6 @@ namespace Site_de_la_Technique_Informatique {
         protected global::System.Web.UI.WebControls.TextBox txtCaractPortatif;
         
         /// <summary>
-        /// Contrôle btnModifierPortable.
-        /// </summary>
-        /// <remarks>
-        /// Champ généré automatiquement.
-        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnModifierPortable;
-        
-        /// <summary>
-        /// Contrôle btnConfirmerPortable.
-        /// </summary>
-        /// <remarks>
-        /// Champ généré automatiquement.
-        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnConfirmerPortable;
-        
-        /// <summary>
-        /// Contrôle btnAnnulerPortable.
-        /// </summary>
-        /// <remarks>
-        /// Champ généré automatiquement.
-        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnAnnulerPortable;
-        
-        /// <summary>
         /// Contrôle txtAutres.
         /// </summary>
         /// <remarks>
@@ -56,15 +29,6 @@ namespace Site_de_la_Technique_Informatique {
         /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox txtAutres;
-        
-        /// <summary>
-        /// Contrôle btnAjouterEvenement.
-        /// </summary>
-        /// <remarks>
-        /// Champ généré automatiquement.
-        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnAjouterEvenement;
         
         /// <summary>
         /// Contrôle lviewEcheancier.
