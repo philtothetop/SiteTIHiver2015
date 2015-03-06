58a3e243cf0d4f98d7af17cba80549b444728b66
CedricArchambault <cedric.archambault@gmail.com>
2015-03-06 08:49:11 -0500

    Inscription Modif

--- Site de la Technique Informatique/Site de la Technique Informatique/Inscription/Inscription.aspx.designer.cs
+++ Site de la Technique Informatique/Site de la Technique Informatique/Inscription/Inscription.aspx.designer.cs
@@ -11,5 +11,32 @@ namespace Site_de_la_Technique_Informatique.Inscription {
     
     
     public partial class Inscription {
+        
+        /// <summary>
+        /// Contrôle ImgExSrc.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField ImgExSrc;
+        
+        /// <summary>
+        /// Contrôle smProxy.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.ScriptManagerProxy smProxy;
+        
+        /// <summary>
+        /// Contrôle lviewFormulaireInscription.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListView lviewFormulaireInscription;
     }
 }
