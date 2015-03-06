f1f38d384e44c9ad708082e5467beb5110166476
Jacob Fontaine <jacob.fontaine@gmail.com>
2015-03-06 09:13:48 -0500

    vfxv

--- Site de la Technique Informatique/Site de la Technique Informatique/Inscription/Validation-Inscription.aspx.designer.cs
+++ Site de la Technique Informatique/Site de la Technique Informatique/Inscription/Validation-Inscription.aspx.designer.cs
@@ -11,5 +11,14 @@ namespace Site_de_la_Technique_Informatique.Inscription {
     
     
     public partial class Validation_Inscription {
+        
+        /// <summary>
+        /// Contrôle lviewValidationInscription.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListView lviewValidationInscription;
     }
 }
