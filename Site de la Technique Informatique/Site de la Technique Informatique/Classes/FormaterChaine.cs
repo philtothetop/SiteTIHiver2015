c281b46c0b34e58ef876dce5cb0b697aa4098fc3
xavpoutre1 <poutrexavier@gmail.com>
2015-03-06 08:45:40 -0500

    heehehe

---
+++ Site de la Technique Informatique/Site de la Technique Informatique/Classes/FormaterChaine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace Site_de_la_Technique_Informatique.Classes
+{
+    public class FormaterChaine
+    {
+        public static string formaterChaine(string text)
+        {
+            var normalizedString = text.Normalize(NormalizationForm.FormD);
+            var stringBuilder = new StringBuilder();
+
+            foreach (var c in normalizedString)
+            {
+                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+                if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+                {
+                    stringBuilder.Append(c);
+                }
+            }
+
+            return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
+        }
+    }
+}
