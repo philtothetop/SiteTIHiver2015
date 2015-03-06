8eaa8625e2487bc775900fed2a48a7e899410911
Karl Gosselin <karlgosselin300@gmail.com>
2015-03-06 14:43:32 -0500

    random

--- Site de la Technique Informatique/Site de la Technique Informatique/QuiSommesNous.aspx.cs
+++ Site de la Technique Informatique/Site de la Technique Informatique/QuiSommesNous.aspx.cs
@@ -3,6 +3,7 @@
 
 using Site_de_la_Technique_Informatique.Model;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,13 +14,22 @@ namespace Site_de_la_Technique_Informatique
 {
     public partial class QuiSommesNous : ErrorHandling
     {
+        static Random rng = new Random();
+        int index = new int();
+        List<Etudiant> lesEtudiants = new List<Etudiant>();
+
         #region Évènements de la page
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            using (LeModelTIContainer lecontexte = new LeModelTIContainer())
+            {
+                index = 0;
+                string PhotoParDefaut = "photobase.bmp";
+                lesEtudiants = (from etudiants in lecontexte.UtilisateurSet.OfType<Membre>().OfType<Etudiant>() where (etudiants.pathPhotoProfil != PhotoParDefaut) select etudiants).ToList();
+                Randomize(lesEtudiants);
+            }
         }
-        #endregion 
-
+        #endregion
 
         #region Remplissage du Listview LvProfesseurs
         public IQueryable<Professeur> lvProfesseurs_GetData()
@@ -41,7 +51,7 @@ namespace Site_de_la_Technique_Informatique
 
         #endregion
 
-        #region Remplissage du ListView lvÉtudiants 
+        #region Remplissage du ListView lvÉtudiants
 
         public IQueryable<Etudiant> lvEtudiants_GetData()
         {
@@ -59,7 +69,36 @@ namespace Site_de_la_Technique_Informatique
             }
             return listeEtudiants.AsQueryable();
         }
-    
+
+        public void Randomize(IList list)
+        {
+            for (int i = list.Count - 1; i > 0; i--)
+            {
+                int swapIndex = rng.Next(i + 1);
+                if (swapIndex != i)
+                {
+                    object tmp = list[swapIndex];
+                    list[swapIndex] = list[i];
+                    list[i] = tmp;
+                }
+            }
+        }
+
+        //VA CHERCHER UN ÉTUDIANT AU RANDOM
+        public Etudiant getUnEtudiantRandom()
+        {
+            Etudiant cestMonTour = new Etudiant();
+            try
+            {
+                cestMonTour = lesEtudiants[index];
+                index++;
+            }
+            catch (Exception ex)
+            {
+                LogErreur("QuiSommesNous-getUnEtudiantRandom", ex);
+            }
+            return cestMonTour;
+        }
 
         #endregion
 
