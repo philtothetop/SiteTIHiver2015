2ac4a78b478a015d93e284d88b10fbbae52e32f9
Philippe Bibeau <bibeaup@gmail.com>
2015-03-20 10:47:52 -0400

    1

--- Site de la Technique Informatique/Site de la Technique Informatique/Site.Master.designer.cs
+++ Site de la Technique Informatique/Site de la Technique Informatique/Site.Master.designer.cs
@@ -67,6 +67,15 @@ namespace Site_de_la_Technique_Informatique {
         protected global::System.Web.UI.WebControls.Label lblEnLigne;
         
         /// <summary>
+        /// Contrôle lnkbtnDeconnexion.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton lnkbtnDeconnexion;
+        
+        /// <summary>
         /// Contrôle upConnexion.
         /// </summary>
         /// <remarks>
