92b691167187b0da750415eb53dc497ab9b3ddde
xavpoutre1 <poutrexavier@gmail.com>
2015-05-08 11:22:34 -0400

    fgdsgsd

--- Site de la Technique Informatique/Site de la Technique Informatique/OffreEmploi.aspx.designer.cs
+++ Site de la Technique Informatique/Site de la Technique Informatique/OffreEmploi.aspx.designer.cs
@@ -49,6 +49,15 @@ namespace Site_de_la_Technique_Informatique {
         protected global::System.Web.UI.WebControls.LinkButton lnkModifier;
         
         /// <summary>
+        /// Contrôle lblDescriptionOffre.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblDescriptionOffre;
+        
+        /// <summary>
         /// Contrôle lblAdresseVille.
         /// </summary>
         /// <remarks>
@@ -137,14 +146,5 @@ namespace Site_de_la_Technique_Informatique {
         /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label lblPersonneRessource;
-        
-        /// <summary>
-        /// Contrôle lblDescriptionOffre.
-        /// </summary>
-        /// <remarks>
-        /// Champ généré automatiquement.
-        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblDescriptionOffre;
     }
 }
