38f9200c8fbe6049a9f9bc1e59b3a1b0cae13d58
Karl Gosselin <karlgosselin300@gmail.com>
2015-05-15 14:48:23 -0400

    kk

--- Site de la Technique Informatique/Site de la Technique Informatique/ErreursImportants.aspx.designer.cs
+++ Site de la Technique Informatique/Site de la Technique Informatique/ErreursImportants.aspx.designer.cs
@@ -20,41 +20,5 @@ namespace Site_de_la_Technique_Informatique {
         /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-        
-        /// <summary>
-        /// Contrôle error.
-        /// </summary>
-        /// <remarks>
-        /// Champ généré automatiquement.
-        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl error;
-        
-        /// <summary>
-        /// Contrôle lblErreurs.
-        /// </summary>
-        /// <remarks>
-        /// Champ généré automatiquement.
-        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblErreurs;
-        
-        /// <summary>
-        /// Contrôle lblInnerTrace.
-        /// </summary>
-        /// <remarks>
-        /// Champ généré automatiquement.
-        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblInnerTrace;
-        
-        /// <summary>
-        /// Contrôle lblStackTrace.
-        /// </summary>
-        /// <remarks>
-        /// Champ généré automatiquement.
-        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblStackTrace;
     }
 }
